c0c44dd754cb108c1696bf9f470cf7ab1a8908f9
OskarHulter <osh@trustpilot.com>
2025-06-26 02:57:40 +0200

    fix: build

--- apps/chart-lounge/next.config.js
+++ apps/chart-lounge/next.config.js
@@ -7,14 +7,14 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/c15t/:path*',
-        destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/c15t/:path*',
+  //       destination: `${process.env.NEXT_PUBLIC_C15T_URL}/:path*`,
+  //     },
+  //   ];
+  // },
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
--- apps/chart-lounge/src/components/layout.tsx
+++ apps/chart-lounge/src/components/layout.tsx
@@ -1,14 +1,3 @@
-import Providers from '@/components/providers';
-import { ConsentManagerDialog, CookieBanner } from '@c15t/nextjs';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <Providers>
-      <main className="app">
-        {children}
-        <CookieBanner />
-        <ConsentManagerDialog />
-      </main>
-    </Providers>
-  );
+  return <main className="app">{children}</main>;
 }
