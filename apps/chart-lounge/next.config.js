a8e405fbdc5e36e16e22a99f1f1c8696be63bb02
OskarHulter <osh@trustpilot.com>
2025-07-14 22:01:54 +0200

    feat: enhance environment configuration and validation; update dependencies

--- apps/chart-lounge/next.config.js
+++ apps/chart-lounge/next.config.js
@@ -1,12 +1,17 @@
 //@ts-check
 
-
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+// const env = require("./src/env.js");
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  reactStrictMode: true,
   // async rewrites() {
   //   return [
   //     {
@@ -17,6 +22,15 @@ const nextConfig = {
   // },
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   nx: {},
 };
 
